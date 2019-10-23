a85236f79cbc54aa08b398b74f9cb58fe7821591
nestoor22 <nestorslavko45@gmail.com>
2019-10-23 23:31:38 +0300

    Refactoring: change variables names, add comments.

--- FilmsScrapping/FilmsScrapping/spiders/films_spider.py
+++ FilmsScrapping/FilmsScrapping/spiders/films_spider.py
@@ -1,7 +1,9 @@
 import scrapy
 
 
+# for running - scrapy crawl ex_fs_net_films_spider
 class ExFsNetFilmsSpider(scrapy.Spider):
+    # Spider name
     name = 'ex_fs_net_films_spider'
 
     def start_requests(self):
@@ -11,25 +13,38 @@ class ExFsNetFilmsSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for link in response.css('div[class="MiniPost"]'):
-            link = link.css('a[class="MiniPostPoster"]::attr(href)').get()
-            yield scrapy.Request(url=link, callback=self.parse_info)
+        for film_page_link in response.css('div[class="MiniPost"]'):
+            # Get link from each mini-poster to film page
+            film_page_link = film_page_link.css('a[class="MiniPostPoster"]::attr(href)').get()
+            yield scrapy.Request(url=film_page_link, callback=self.parse_info)
+
+        # Get next page if exist and make recursion call
         if response.css('div[class="navigations"]'):
-            next_link = response.css('div[class="navigations"] a::attr(href)').getall()[-1]
-            yield scrapy.Request(url=next_link, callback=self.parse)
+            next_page_link = response.css('div[class="navigations"] a::attr(href)').getall()[-1]
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
 
     def parse_info(self, response):
-        info = dict()
-        info['name_rus'] = response.css('h1[class="view-caption"]::text').get()
-        info['name_eng'] = response.css('h2[class="view-caption2"]::text').get()
-        info['imdb_rating'] = float(response.css('div[class="in_kp_imdb"] div[class="in_name_imdb"]::text').get())
+        # Create a dict for storing information
+        information_about_film = dict()
+
+        # Save film name in english and russian.
+        information_about_film['name_rus'] = response.css('h1[class="view-caption"]::text').get()
+        information_about_film['name_eng'] = response.css('h2[class="view-caption2"]::text').get()
+
+        # Add IMDB rating
+        information_about_film['imdb_rating'] = float(response.css('div[class="in_kp_imdb"] '
+                                                                   'div[class="in_name_imdb"]::text').get())
+
         story_info_keys = response.css('div[class="FullstoryInfo"] h4[class="FullstoryInfoTitle"]::text').getall()
         story_info_values = response.css('div[class="FullstoryInfo"] p[class="FullstoryInfoin"]')
-        i = 0
-        info['info'] = {}
+
+        # Story information about years of release date, country and genre
+        information_index = 0
+        information_about_film['info'] = {}
         for one_select in story_info_values:
             one_property = one_select.css('a::text').getall()
-            if one_select.css('a::text').getall():
-                info['info'][story_info_keys[i]] = one_property
-            i += 1
-        yield info
+            if one_property:
+                information_about_film['info'][story_info_keys[information_index]] = one_property
+            information_index += 1
+
+        yield information_about_film

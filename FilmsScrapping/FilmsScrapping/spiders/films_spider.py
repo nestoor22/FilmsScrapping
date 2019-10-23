1c9f9f39ef28c13fd0f8c153ed6b67f6c2780fe6
nestoor22 <nestorslavko45@gmail.com>
2019-10-23 23:14:45 +0300

    Parse information about imdb rating, year, countries, genre

--- FilmsScrapping/FilmsScrapping/spiders/films_spider.py
+++ FilmsScrapping/FilmsScrapping/spiders/films_spider.py
@@ -22,4 +22,14 @@ class ExFsNetFilmsSpider(scrapy.Spider):
         info = dict()
         info['name_rus'] = response.css('h1[class="view-caption"]::text').get()
         info['name_eng'] = response.css('h2[class="view-caption2"]::text').get()
+        info['imdb_rating'] = float(response.css('div[class="in_kp_imdb"] div[class="in_name_imdb"]::text').get())
+        story_info_keys = response.css('div[class="FullstoryInfo"] h4[class="FullstoryInfoTitle"]::text').getall()
+        story_info_values = response.css('div[class="FullstoryInfo"] p[class="FullstoryInfoin"]')
+        i = 0
+        info['info'] = {}
+        for one_select in story_info_values:
+            one_property = one_select.css('a::text').getall()
+            if one_select.css('a::text').getall():
+                info['info'][story_info_keys[i]] = one_property
+            i += 1
         yield info
